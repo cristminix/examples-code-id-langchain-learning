f41f503cceb494a9339f39f6f90e4310eaa4e694
Putra Budiman <cristminix@gmail.com>
2025-12-16 19:33:56 +0700

    update python version

--- src/ch01/ch01.ts
+++ src/ch01/ch01.ts
@@ -1,35 +1,21 @@
 import { config } from "dotenv"
-import {
-  chatModelBasic,
-  chatModelWithSystemInstruction,
-} from "./chatModelBasic"
-import {
-  chatPromptTemplateBasic,
-  promptTemplateBasic,
-} from "./promptTemplateBasic"
-import {
-  commaSeparatedBasic,
-  structuredOutputBasic,
-} from "./structuredOutputBasic"
+import { chatModelBasic, chatModelWithSystemInstruction } from "./chatModelBasic"
+import { chatPromptTemplateBasic, promptTemplateBasic } from "./promptTemplateBasic"
+import { commaSeparatedBasic, structuredOutputBasic } from "./structuredOutputBasic"
 import { runableBasic } from "./runableBasic"
-import {
-  chainBasic,
-  chainBasic2,
-  compositionBasic,
-  compositionBasic2,
-} from "./compositionBasic"
+import { chainBasic, chainBasic2, compositionBasic, compositionBasic2 } from "./compositionBasic"
 config()
 export const main = async () => {
   // await chatModelBasic()
   // await chatModelWithSystemInstruction()
   // await promptTemplateBasic()
   // await chatPromptTemplateBasic()
-  // await structuredOutputBasic()
+  await structuredOutputBasic()
   // await commaSeparatedBasic()
   // await runableBasic()
   // await compositionBasic()
   // await compositionBasic2()
   // await chainBasic()
-  await chainBasic2()
+  // await chainBasic2()
 }
 main().catch((e) => console.error(e))
